ac9f3a446d4538b64d72e0bba9025a5992643996
bernardoalbano <albanobernardo@live.com.pt>
2022-10-07 11:09:37 +0000

    Adicionando ficheiros

---
+++ iac2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Criando directorios e sub-directorios.............."
+
+mkdir futebolangola
+cd futebolangola
+mkdir LD HB BG
+cd LD
+touch kabuscorp dagosto progresso
+cd ../HB
+touch caala petro kuito
+cd ../BG
+touch maio canjala kileva
+
+echo "Fim..........."
